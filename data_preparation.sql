27020d96ee48995b471ddb96e7e44b6ba7dcff7f
Emily Sung <63834866+emilymjsung@users.noreply.github.com>
2022-09-06 14:55:38 -0600

    Create data_preparation.sql

---
+++ data_preparation.sql
@@ -0,0 +1,189 @@
+-- [Table 1: dbo.pro_historical]
+
+-- 1. Data check
+
+-- 1-1. Import data from 6 split CSV files and join them as a new table (data doesn't show correctly when uploaded as a whole in SQL Server v18.11.1)
+-- function (to split a CSV file into multiple smaller files) code source: 
+-- https://python.plainenglish.io/split-a-large-csv-file-randomly-or-equally-into-smaller-files-76f7bb734459
+
+SELECT  *
+INTO		yeg_property.dbo.pro_historical
+FROM		yeg_property.dbo.pro_historical_1 
+UNION ALL	
+SELECT	*
+FROM		yeg_property.dbo.pro_historical_2
+UNION ALL
+SELECT	*
+FROM		yeg_property.dbo.pro_historical_3 
+UNION ALL
+SELECT	*
+FROM		yeg_property.dbo.pro_historical_4
+UNION ALL
+SELECT	*
+FROM		yeg_property.dbo.pro_historical_5
+UNION ALL
+SELECT	*
+FROM		yeg_property.dbo.pro_historical_6
+
+-- 1-2. Total row count vs. Unique row count: 3,798,629 vs. 423,114
+
+SELECT	COUNT([Account Number]) AS property_count
+	      , COUNT(DISTINCT([Account Number])) AS unique_property_count
+FROM	  yeg_property.dbo.pro_historical;
+
+-- 1-3. Check duplicates in 2 ways
+
+-- 1-3-1. Check with HAVING: each account should have 10 records for each year, 2012-2021 / no duplicates found
+
+SELECT		[Account Number]
+		      , COUNT(*) AS count
+FROM		  yeg_property.dbo.pro_historical
+GROUP BY	[Account Number]
+HAVING		COUNT(*) > 10
+ORDER BY	COUNT(*) DESC;
+
+-- 1-3-2. Check with DISTINCT: 337,298 accounts in 2012 and 413,809 accounts in 2021 / no duplicates found
+
+SELECT	COUNT([Account Number]) AS pro_count_2012
+	      , COUNT(DISTINCT([Account Number])) AS unique_pro_count_2012
+FROM	  yeg_property.dbo.pro_historical
+WHERE	  [Assessment Year] = 2012;
+GO
+SELECT	COUNT([Account Number]) AS pro_count_2021
+	      , COUNT(DISTINCT([Account Number])) AS unique_pro_count_2021
+FROM	  yeg_property.dbo.pro_historical
+WHERE	  [Assessment Year] = 2021;
+
+-- 2. Data Preparation
+
+-- 2-1. Create a new table "pro_historical_res" that only contains residential properties and selected columns for the analysis 
+
+SELECT	[Account Number]
+	      , [Assessment Year]
+	      , [House Number]
+	      , [Street Name] 
+	      , Latitude
+	      , Longitude
+	      , Neighbourhood
+	      , [Actual Year Built] 
+	      , Garage
+	      , Zoning
+	      , [Lot Size]
+	      , [Assessed Value] 
+INTO	  yeg_property.dbo.pro_historical_res 
+FROM	  yeg_property.dbo.pro_historical
+WHERE	  [Assessed Value] IS NOT NULL 
+	      AND [Assessed Value] <> 0
+	      AND (([Assessment Class 1] = 'RESIDENTIAL' AND [Assessment Class % 1] > 50)
+		    OR ([Assessment Class 2] = 'RESIDENTIAL' AND [Assessment Class % 2] > 50)
+		    OR ([Assessment Class 3] = 'RESIDENTIAL' AND [Assessment Class % 3] > 50));
+
+-- count the rows: 3,541,140 rows / 395,355 unique rows
+
+SELECT	COUNT([Account Number]) AS pro_res_count
+    	  , COUNT(DISTINCT([Account Number])) AS unique_pro_res_count
+FROM	  yeg_property.dbo.pro_historical_res;
+
+-- 2-2. Change “Y/N” to “Yes/No” in the Garage column
+-- 3,541,140 rows affected. No null values in the Garage column.
+
+UPDATE  yeg_property.dbo.pro_historical_res
+SET     Garage = CASE	WHEN Garage = 'Y' THEN 'Yes'
+			  WHEN Garage = 'N' THEN 'No' END;
+
+-- 2-3. Build InitCap function, change and update the string format of the columns, [Neighbourhood] and [Street Name]
+
+-- 2-3-1. Build InitCap function
+-- function code source: http://www.sql-server-helper.com/functions/initcap.aspx
+
+CREATE FUNCTION [dbo].[InitCap] ( @InputString varchar(4000) ) 
+RETURNS VARCHAR(4000)
+AS
+BEGIN
+
+DECLARE @Index          INT
+DECLARE @Char           CHAR(1)
+DECLARE @PrevChar       CHAR(1)
+DECLARE @OutputString   VARCHAR(250)
+
+SET @OutputString = LOWER(@InputString)
+SET @Index = 1
+
+WHILE @Index <= LEN(@InputString)
+BEGIN
+    SET @Char     = SUBSTRING(@InputString, @Index, 1)
+    SET @PrevChar = CASE WHEN @Index = 1 THEN ' '
+                         ELSE SUBSTRING(@InputString, @Index - 1, 1)
+                    END
+
+    IF @PrevChar IN (' ', ';', ':', '!', '?', ',', '.', '_', '-', '/', '&', '''', '(')
+    BEGIN
+        IF @PrevChar != '''' OR UPPER(@Char) != 'S'
+            SET @OutputString = STUFF(@OutputString, @Index, 1, UPPER(@Char))
+    END
+
+    SET @Index = @Index + 1
+END
+
+RETURN @OutputString
+
+END
+GO
+
+-- 2-3-2. Change the format of the column [Neighbourhood] and update the table (e.g., TWIN BROOKS -> Twin Brooks)
+
+UPDATE  yeg_property.dbo.pro_historical_res
+SET     [Neighbourhood] = dbo.InitCap([Neighbourhood]);
+
+-- 2-3-3. Change the format of the column [Street Name] and update the table (e.g., 111 AVENUE NW -> 111 Avenue NW)
+
+UPDATE  yeg_property.dbo.pro_historical_res
+SET     [Street Name] = dbo.InitCap(LEFT([Street Name], LEN([Street Name]) - 3)) + ' ' + UPPER(RIGHT([Street Name], 2))
+
+------------------------------------------------------------------------------------------------------------------------------------------
+
+-- [Table 2: dbo.neighbourhoods]
+
+-- 1. Data check
+
+-- 1-1. Total row count vs. Unique row count: 410
+-- we see some invalid neighbourhood names that seem to be a part of the column, [Geometry Multipolygon]
+
+SELECT	COUNT([Descriptive Name]) AS nbh_count
+			  , COUNT(DISTINCT([Descriptive Name])) AS unique_nbh_count
+FROM		[yeg_property].[dbo].[neighbourhoods];
+
+-- 1-2. Check nulls in the 2 columns we need, [Descriptive Name] and [Geometry Multipolygon]: no nulls
+
+SELECT	*
+FROM		[yeg_property].[dbo].[neighbourhoods]
+WHERE		[Descriptive Name] IS NULL OR [Geometry Multipolygon] IS NULL;
+
+
+-- 2. Data cleaning
+
+-- 2-1. Drop the rows with the invalid data
+
+-- check the rows where the value of [Descriptive Name] contains numbers only: 8 rows
+
+SELECT	*
+FROM		[yeg_property].[dbo].[neighbourhoods]
+WHERE		[Descriptive Name] LIKE '%[0-9]';
+
+-- check the rows where the value of [Neighbourhood Name] contains numbers only: 8 rows
+
+SELECT	*
+FROM		[yeg_property].[dbo].[neighbourhoods]
+WHERE		[Neighbourhood Name] LIKE '%[0-9]';
+
+-- drop the rows with the invalid data: 8 rows affected
+
+DELETE	
+FROM	[yeg_property].[dbo].[neighbourhoods]
+WHERE	[Descriptive Name] LIKE '%[0-9]';
+
+-- confirm the updated row counts: 402
+
+SELECT	COUNT([Descriptive Name]) AS nbh_count
+			  , COUNT(DISTINCT([Descriptive Name])) AS unique_nbh_count
+FROM		[yeg_property].[dbo].[neighbourhoods];
