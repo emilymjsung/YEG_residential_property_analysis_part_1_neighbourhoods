b2431cba0b3708b597f2796cbd57a0e5eba68cf8
Emily Sung <63834866+emilymjsung@users.noreply.github.com>
2022-09-09 10:42:25 -0600

    Update data_preparation.sql

--- data_preparation.sql
+++ data_preparation.sql
@@ -6,29 +6,29 @@
 -- function (to split a CSV file into multiple smaller files) code source: 
 -- https://python.plainenglish.io/split-a-large-csv-file-randomly-or-equally-into-smaller-files-76f7bb734459
 
-SELECT  *
+SELECT  	*
 INTO		yeg_property.dbo.pro_historical
 FROM		yeg_property.dbo.pro_historical_1 
 UNION ALL	
-SELECT	*
+SELECT		*
 FROM		yeg_property.dbo.pro_historical_2
 UNION ALL
-SELECT	*
+SELECT		*
 FROM		yeg_property.dbo.pro_historical_3 
 UNION ALL
-SELECT	*
+SELECT		*
 FROM		yeg_property.dbo.pro_historical_4
 UNION ALL
-SELECT	*
+SELECT		*
 FROM		yeg_property.dbo.pro_historical_5
 UNION ALL
-SELECT	*
+SELECT		*
 FROM		yeg_property.dbo.pro_historical_6
 
 -- 1-2. Total row count vs. Unique row count: 3,798,629 vs. 423,114
 
 SELECT	COUNT([Account Number]) AS property_count
-	, COUNT(DISTINCT([Account Number])) AS unique_property_count
+		, COUNT(DISTINCT([Account Number])) AS unique_property_count
 FROM	yeg_property.dbo.pro_historical;
 
 -- 1-3. Check duplicates in 2 ways
@@ -36,7 +36,7 @@ FROM	yeg_property.dbo.pro_historical;
 -- 1-3-1. Check with HAVING: each account should have 10 records for each year, 2012-2021 / no duplicates found
 
 SELECT		[Account Number]
-		, COUNT(*) AS count
+			, COUNT(*) AS count
 FROM		yeg_property.dbo.pro_historical
 GROUP BY	[Account Number]
 HAVING		COUNT(*) > 10
@@ -45,12 +45,12 @@ ORDER BY	COUNT(*) DESC;
 -- 1-3-2. Check with DISTINCT: 337,298 accounts in 2012 and 413,809 accounts in 2021 / no duplicates found
 
 SELECT	COUNT([Account Number]) AS pro_count_2012
-	, COUNT(DISTINCT([Account Number])) AS unique_pro_count_2012
+		, COUNT(DISTINCT([Account Number])) AS unique_pro_count_2012
 FROM	yeg_property.dbo.pro_historical
 WHERE	[Assessment Year] = 2012;
 GO
 SELECT	COUNT([Account Number]) AS pro_count_2021
-	, COUNT(DISTINCT([Account Number])) AS unique_pro_count_2021
+		, COUNT(DISTINCT([Account Number])) AS unique_pro_count_2021
 FROM	yeg_property.dbo.pro_historical
 WHERE	[Assessment Year] = 2021;
 
@@ -59,24 +59,24 @@ WHERE	[Assessment Year] = 2021;
 -- 2-1. Create a new table "pro_historical_res" that only contains residential properties and selected columns for the analysis 
 
 SELECT	[Account Number]
-	, [Assessment Year]
-	, [House Number]
-	, [Street Name] 
-	, Latitude
-	, Longitude
-	, Neighbourhood
-	, [Actual Year Built] 
-	, Garage
-	, Zoning
-	, [Lot Size]
-	, [Assessed Value] 
+		, [Assessment Year]
+		, [House Number]
+		, [Street Name] 
+		, Latitude
+		, Longitude
+		, Neighbourhood
+		, [Actual Year Built] 
+		, Garage
+		, Zoning
+		, [Lot Size]
+		, [Assessed Value] 
 INTO	yeg_property.dbo.pro_historical_res 
 FROM	yeg_property.dbo.pro_historical
 WHERE	[Assessed Value] IS NOT NULL 
 	AND [Assessed Value] <> 0
 	AND (([Assessment Class 1] = 'RESIDENTIAL' AND [Assessment Class % 1] > 50)
-	OR ([Assessment Class 2] = 'RESIDENTIAL' AND [Assessment Class % 2] > 50)
-	OR ([Assessment Class 3] = 'RESIDENTIAL' AND [Assessment Class % 3] > 50));
+	OR 	([Assessment Class 2] = 'RESIDENTIAL' AND [Assessment Class % 2] > 50)
+	OR 	([Assessment Class 3] = 'RESIDENTIAL' AND [Assessment Class % 3] > 50));
 
 -- count the rows: 3,541,140 rows / 395,355 unique rows
 
@@ -89,7 +89,7 @@ FROM	yeg_property.dbo.pro_historical_res;
 
 UPDATE  yeg_property.dbo.pro_historical_res
 SET     Garage = CASE	WHEN Garage = 'Y' THEN 'Yes'
-			WHEN Garage = 'N' THEN 'No' END;
+						WHEN Garage = 'N' THEN 'No' END;
 
 -- 2-3. Build InitCap function, change and update the string format of the columns, [Neighbourhood] and [Street Name]
 
@@ -154,12 +154,12 @@ SET     [Street Name] = dbo.InitCap(LEFT([Street Name], LEN([Street Name]) - 3))
 
 -- 1-1. Total row count vs. Unique row count: 402
 
-SELECT		COUNT([Descriptive_Name]) AS nbh_count
-			, COUNT(DISTINCT([Descriptive_Name])) AS unique_nbh_count
-FROM		[yeg_property].[dbo].[neighbourhoods];
+SELECT	COUNT([Descriptive_Name]) AS nbh_count
+		, COUNT(DISTINCT([Descriptive_Name])) AS unique_nbh_count
+FROM	[yeg_property].[dbo].[neighbourhoods];
 
 -- 1-2. Check nulls: no null values found
 
-SELECT		*
-FROM		[yeg_property].[dbo].[neighbourhoods]
-WHERE		[Descriptive_Name] IS NULL OR [Geometry_Multipolygon] IS NULL;
+SELECT	*
+FROM	[yeg_property].[dbo].[neighbourhoods]
+WHERE	[Descriptive_Name] IS NULL OR [Geometry_Multipolygon] IS NULL;
